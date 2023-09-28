3890b679e9dd0d72569a636a5b53efb5164da211
noViceMin <admin@isamin.kr>
2023-09-29 03:34:57 +0900

    fix: button link

--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,26 @@
+const storeLink = document.querySelector('#header #link');
 const installButtonText = document.querySelector('#button #text');
 const installButtonIcon = document.querySelector('#button #icon');
 
 switch (getMobileOperatingSystem()) {
   case 'iOS':
+    storeLink.href = 'https://redirect.dimigo.in/app-ios';
+    storeLink.target = '_blank';
     installButtonText.textContent = '앱스토어에서 받기';
     installButtonIcon.src = '/svg/ios-button.svg';
     break;
   case 'Android':
+    storeLink.href = 'https://redirect.dimigo.in/app-aos';
+    storeLink.target = '_blank';
     installButtonText.textContent = '플레이스토어에서 받기';
     installButtonIcon.src = '/svg/aos-button.svg';
     break;
   default:
+    storeLink.href = 'javascript:void(0);';
+    storeLink.addEventListener('click', () => {
+      alert('웹 버전은 현재 준비중입니다!');
+      return false;
+    });
     installButtonText.textContent = '웹 버전 디미고인 사용하기';
     installButtonIcon.src = '/svg/web-button.svg';
     break;
